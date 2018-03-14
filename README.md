4311bd7313b25e2885a5eba68984f45e2c340dc0
Unknown <mvivant@3drudder.com>
2018-03-14 11:04:54 +0100

    First function

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # scratch-3dRudder-extension
 
 https://github.com/LLK/scratchx/wiki#introduction
+
+http://scratchx.org/#scratch => http://scratchx.org?url=https://3drudder.github.io/scratch-3dRudder-extension/leapmotion_extension.js
--- extension.js
+++ extension.js
@@ -1,40 +1,69 @@
 (function(ext) {
-    var alarm_went_off = false; // This becomes true after the alarm goes off
-
+   
+    var SDK = null;
+    function load3dRudderjs() {
+        $.getScript('https://3drudder.github.io/3dRudder-js/dist/3dRudder-1.0.4.js')
+            .done(function(script, textStatus) {
+                console.log('Loaded 3dRudder-js');
+                begin();                
+            })
+            .fail(function(jqxhr, settings, exception) {
+                console.log('Error loading 3dRudder-js');
+                load3dRudderjs();
+        });
+    }
+    function begin() {
+        SDK = new Sdk3dRudder();
+        SDK.init();
+        SDK.on('init', function(init) {
+            console.log('3dRudder initialized on Scratch X');            
+        });
+    }
     // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
+    ext._shutdown = function() {
+        if (SDK)
+            SDK.stop();
+    };
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        if(!SDK) 
+            return {status: 1, msg: 'SDK not loaded'};
+        else if (!SDK.controllers[0].connected)
+            return {status: 1, msg: '3dRudder not connected'};
+        else
+            return {status: 2, msg: '3dRudder connected'};
     };
 
-    ext.set_alarm = function(time) {
-       window.setTimeout(function() {
-           alarm_went_off = true;
-       }, time*1000);
-    };
-
-    ext.when_alarm = function() {
-       // Reset alarm_went_off if it is true, and return true
-       // otherwise, return false.
-       if (alarm_went_off === true) {
-           alarm_went_off = false;
-           return true;
-       }
-
-       return false;
-    };
+    ext.is3dRudderConnected = function(port) {
+        if (SDK)
+            return SDK.controllers[port].connected;
+        return false;
+      };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['', 'run alarm after %n seconds', 'set_alarm', '2'],
-            ['h', 'when alarm goes off', 'when_alarm'],
-        ]
+            ['b', '3dRudder %n is Connected', 'is3dRudderConnected', 0]
+            /*['r', '3dRudder_Version', '3dRudder_Version'],
+            ['r', '3dRudder_Status_Code', '3dRudder_Status_Code'],
+            ['r', '3dRudder_Status', '3dRudder_Status'],
+            ['r', '3dRudder_Axis_AX', '3dRudder_Axis_AX'],
+            ['r', '3dRudder_Axis_AY', '3dRudder_Axis_AY'],
+            ['r', '3dRudder_Axis_AZ', '3dRudder_Axis_AZ'],
+            ['r', '3dRudder_Axis_RZ', '3dRudder_Axis_RZ'],
+            ['', '3dRudder_PlaySnd( %s,%b )', '3dRudder_PlaySnd', '', false]*/
+        ],
+        menus: {
+            error: ["Success", "Not connected", "Fail", "Incorrect command", "Timeout", "Wrong signature", "Not ready"],
+            status: ["No status", "No foot stay still", "Initialisation", "Put your feet", "Put second foot", "Stay still", "In use", "Extended mode"]
+        },
+        url: 'https://3drudder.github.io/scratch-3dRudder-extension/',
+        displayName: '3dRudder Extension 1.0'
     };
 
     // Register the extension
-    ScratchExtensions.register('Alarm extension', descriptor, ext);
+    ScratchExtensions.register('3dRudder extension', descriptor, ext);
+    load3dRudderjs();
 })({});
